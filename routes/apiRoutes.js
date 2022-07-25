c22ef925b2286d871102e0aa8079c2334af992be
farhan <farhanhoque764@yahoo.com>
2022-07-24 21:29:44 -0500

    modified apiRoute

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 // creates a random id
 const uuid = require("uuid");
 // brings in the DB class object
-const DB = require("../db/DB");
+const DB = require("../db/db");
 
 // route to get notes
 router.get("/api/notes", async function (req, res) {
